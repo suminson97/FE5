17d418162f31cf88ed028148ab7fbe9d8e07ecbc
suminson <sabi1111@nate.com>
2023-05-22 22:10:15 +0900

    20230522

---
+++ React/20230522/vanilla-redux/src/components/GoodsCounter.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addNumber, substractNumber } from "../modules/goodsCounter";
+
+//state 조회
+export default function GoodsCounter() {
+  const { stock, goods } = useSelector((state) => {
+    return { stock: state.goodsReducer.stock, goods: state.goodsReducer.goods };
+  });
+
+  const dispatch = useDispatch();
+
+  //dispatch 실행
+  const onAddNumber = () => dispatch(addNumber());
+  const onSubstractNumber = () => dispatch(substractNumber());
+
+  return (
+    <div>
+      <h2>딥러닝 개발자 무릎 담요</h2>
+      <span>
+        <strong>17,500</strong>원
+      </span>
+      <div>
+        <button type="button" onClick={onSubstractNumber}>
+          Minus
+        </button>
+        <span>{goods}</span>
+        <button onClick={onAddNumber}>Plus</button>
+      </div>
+      <div>
+        총 수량 <strong>{goods}</strong>
+      </div>
+      <div>
+        <strong>{goods * 17500}</strong>원
+      </div>
+      <div>
+        제고 <strong>{stock}</strong>
+      </div>
+    </div>
+  );
+}
