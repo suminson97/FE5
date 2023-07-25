0c6671c35df329db0d1335129af703390c43773c
suminson <sabi1111@nate.com>
2023-07-25 22:52:40 +0900

    리액트 중급반 특강 2

---
+++ ReactServer/src/components/Home.js
@@ -0,0 +1,12 @@
+import { divide } from "lodash";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>this is home</h1>
+      <button onClick={() => console.log("clicked")}>버튼입니다.</button>
+    </div>
+  );
+};
+export default Home;
